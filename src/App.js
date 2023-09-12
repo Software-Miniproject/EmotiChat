f07e983bec047bc03f57c187fd06dab865e5cc39
Trevor Chan <trevchan@bu.edu>
2023-09-11 21:16:45 -0400

    added in google authentication button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
+import { google_signin } from "./firebase";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={google_signin} type="button" class="login-with-google-btn" >
+        Sign in with Google
+      </button>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,4 +20,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);
+export const auth = getAuth(app);
+
+// Initialize requirements for using Google for authentication
+const auth_provider = new GoogleAuthProvider()
+export const google_signin = () => {
+  signInWithPopup(auth, auth_provider)
+    .then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error);
+    });
+}
