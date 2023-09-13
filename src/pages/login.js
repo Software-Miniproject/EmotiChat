6f5b6b5f3c207643ef8e59a9e5befec958bd8bc8
Trevor Chan <trevchan@bu.edu>
2023-09-13 16:00:50 -0400

    Added navbar to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { google_signin } from "../firebase";
+import NavBar from "../components/Navbar";
 const login = () => {
 
     return (
         <div className="App">
+            <NavBar />
             <button onClick={google_signin} type="button" class="login-with-google-btn" >
                 Sign in with Google
             </button>
