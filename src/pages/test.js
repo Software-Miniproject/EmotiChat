89519307e468853dd599f87c079938a26d263d04
Trevor Chan <trevchan@bu.edu>
2023-09-13 22:34:45 -0400

    Started implementing useAuthState and added in sign out button

---
+++ src/pages/test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/Navbar";
+import SendMsg from "../components/SendMsg";
+
+const test = () => {
+
+    return (
+        <div className="App">
+            <NavBar/>
+            <SendMsg/>
+        </div>
+            
+    )
+}
+export default test;
