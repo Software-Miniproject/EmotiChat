6b340c35390cf7d18bffe00e5a7dd754cbace558
Andres Garcia <andresfg@bu.edu>
2023-09-14 21:06:42 -0400

    corrected routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Home from "./pages/home";
 import login from "./pages/login";
 import test from "./pages/test";
+import account from "./pages/account";
 
 function App() {
   return (
-    <BrowserRouter>
+   <div>
       <Routes>
         <Route exact path="/" Component={Home} />
         <Route exact path="/login" Component={login}/>
         <Route exact path="/test" Component={test}/>
+        <Route exact path="/account" Compnent={account}/>
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
