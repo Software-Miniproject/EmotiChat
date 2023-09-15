f90db59440c7d0528927d41fe8669d403a59d658
kristieg <91104705+kristieg@users.noreply.github.com>
2023-09-15 10:21:36 -0400

    Update App.js
    added page to navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import login from "./pages/login";
+import search from "./pages/search";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" Component={Home} />
         <Route exact path="/login" Component={login}/>
+        <Route exact path="/search" Component={search}/>
       </Routes>
     </BrowserRouter>
   );
