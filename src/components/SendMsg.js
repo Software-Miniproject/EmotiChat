4bffb14b86a0e42ace3b8a1f69bedefad14c6be6
Trevor Chan <trevchan@bu.edu>
2023-09-16 16:09:22 -0400

    Removed handleSubmit

---
+++ src/components/SendMsg.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { auth, db } from "../firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+
+// Function used to send messages by storing the information into firestore
+const SendMsg = async(event) => {
+    event.preventDefault();
+    const [message, setMessage] = useState("");
+    return (
+        <form className="send-message">
+            <label htmlFor="messageIn" hidden>
+                Enter Message
+            </label>
+            <input
+                id="messageIn"
+                name="messageIn"
+                type="text"
+                className="form-input__input"
+                placeholder="type message..."
+                value={message}
+                // Dynamically set the message that will be sent to what's currently in the box
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <button type="submit">Send</button>
+        </form>
+    );
+};
+
+export default SendMsg;
