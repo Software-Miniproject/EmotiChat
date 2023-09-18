1559827ae29c83e6d54eb1402963bbacbd74b2ad
Andres Garcia <andresfg@bu.edu>
2023-09-18 00:53:07 -0400

    User data now stored only after very first login

--- src/components/StoreUser.js
+++ src/components/StoreUser.js
@@ -1,17 +1,27 @@
 import { db } from '../firebase';
-import { collection, addDoc, doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
 
-// Function to store user data in Firestore
+// Function to store user data in Firestore, checking for existing document
 export const storeUserData = async (uid, email, displayName, photoURL) => {
   try {
     const userRef = doc(db, 'users', uid);
-    await setDoc(userRef, {
-      email: email,
-      name: displayName,
-      uid: uid,
-      pfp: photoURL
-    });
-    console.log('User data stored successfully');
+
+    // Check if the document already exists
+    const userDoc = await getDoc(userRef);
+
+    if (!userDoc.exists()) {
+      // Document doesn't exist, so create it
+      await setDoc(userRef, {
+        email: email,
+        name: displayName,
+        uid: uid,
+        pfp: photoURL
+      });
+      console.log('User data stored successfully');
+    } else {
+      // Document already exists, do nothing
+      console.log('User data already exists in Firestore');
+    }
   } catch (error) {
     console.error('Error storing user data:', error);
   }
