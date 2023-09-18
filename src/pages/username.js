44cdccd5e37d537f197ce491d99dd8430e1768fb
Andres Garcia <andresfg@bu.edu>
2023-09-18 17:01:39 -0400

    Fixed username setting error

--- src/pages/username.js
+++ src/pages/username.js
@@ -69,50 +69,51 @@ const Username = () => {
                 setErrorMessage('Username cannot be empty');
                 return;
             }
-
+    
             // Check if the username is already in use by another user
             if (existingUsernames.includes(newUsername)) {
                 setErrorMessage('Username is already in use by another user');
                 return;
             }
-
+    
             // Check if the username matches any other user's email prefix
             if (otherUserEmailPrefixes.includes(newUsername)) {
                 setErrorMessage('Username is already in use by another user');
                 return;
             }
-
+    
             // Check if the username is valid (contains only letters, numbers, ., and _ and is no longer than 30 characters)
             const isValidUsername = /^[a-zA-Z0-9._]{3,30}$/.test(newUsername);
-
+    
             if (!isValidUsername) {
                 setErrorMessage('Invalid username format. It can only contain letters, numbers, ., and _ and must be 3 to 30 characters long.');
                 return;
             }
-
+    
             // Update the username in the Firestore document
             const uid = user.uid;
             const userRef = doc(db, 'users', uid);
-
+    
             await updateDoc(userRef, {
                 username: newUsername
             });
-
+    
             // Update the username property in the user state
             setUser(prevUser => ({ ...prevUser, username: newUsername }));
-
+    
             setSuccessMessage('Username updated successfully');
-
+    
             // Clear success message after 2 seconds
             setTimeout(() => setSuccessMessage(''), 2000);
         } catch (error) {
             console.error('Error updating username:', error);
             setErrorMessage('Error updating username: ' + error.message); // Include error message in the error state
-
+    
             // Clear error message after 2 seconds
             setTimeout(() => setErrorMessage(''), 2000);
         }
     };
+    
 
     return (
         <div className='Username'>
