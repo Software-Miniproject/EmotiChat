7f1ddd92b4cffdb68f125854b7ddd8271e6f21b6
kristieg <91104705+kristieg@users.noreply.github.com>
2023-09-19 17:36:37 -0400

    Update search.js
    re-update

--- src/pages/search.js
+++ src/pages/search.js
@@ -5,15 +5,15 @@ import { query, where, collection, getDocs } from "firebase/firestore";
 const Search = () => {
     const [users, setUsers] = useState();
     const [userInput, setUserInput] = useState('');
+
     const handleButtonClick = () => {
       const inputValue = document.getElementById('userInputField').value;
       setUserInput(inputValue);
     }
 
-    const usersRef = collection(db, "users");
-    const q = query(usersRef, where("name", "==", userInput));
-
     const getUsers = async () => {
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, where("name", ">=", userInput));
         const querySnapshot = await getDocs(q);
         const users = querySnapshot.docs.map(doc => ({id: doc.id, ... doc.data()}))
         setUsers(users);
@@ -27,39 +27,38 @@ const Search = () => {
     return (
         <div>
             <br></br>
-    <div>
-      <p>Search: {userInput}</p>
-      <input type="text" id="userInputField" placeholder="Exact Name..."
-      /><button onClick={handleButtonClick}>Submit</button>
-    </div>
-            <br></br>
-            <br></br>
-            <table className="striped-table">
-                <thead>
-                    <tr>
-                        <th>Profile</th>
-                        <th>Name</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users ? (
-                        users.map((users) => (
-                            <tr key={users.uid}>
-                                <td>{users.pfp && <img src={users.pfp} alt="" />}</td>
-                                <td>{users.name}</td>
-                                <td>{users.username}</td>
-                                <td>{users.email}</td>
-                            </tr>
-                        ))
-                    ) : (
-                        <tr>
-                            <td colSpan={40}></td>
+            <div>
+                <input type="text" id="userInputField" placeholder="Enter Search..."/>
+                <button onClick={handleButtonClick}>Submit</button>
+                <br></br>
+            </div>
+        <br></br>
+        <table className="striped-table">
+            <thead>
+                <tr>
+                    <th>Profile</th>
+                    <th>Name</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users ? (
+                    users.map((users) => (
+                        <tr key={users.uid}>
+                            <td>{users.pfp && <img src={users.pfp} alt="" />}</td>
+                            <td>{users.name}</td>
+                            <td>{users.username}</td>
+                            <td>{users.email}</td>
                         </tr>
-                    )}
-                </tbody>
-            </table>
+                    ))
+                ) : (
+                    <tr>
+                        <td colSpan={40}></td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
         </div>
     );
 };
